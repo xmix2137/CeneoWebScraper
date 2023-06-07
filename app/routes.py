5071fb4588abf5785b87809ee339bacdb061bfbd
xmix2137 <226458@student.uek.krakow.pl>
2023-06-07 12:59:42 +0200

    2023-06-07 KS

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Krzysztof Sikora!"
+    return render_template('index.html')
 
-@app.route('/name/', deafults={'name': None})
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
